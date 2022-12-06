0108e397bf98276656085195103e96c79d5ff729
salinas171605@gmail.com <salinas171605@gmail.com>
2022-12-05 18:08:45 -0600

    actualizacion css

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class crearTarea(forms.Form):
     title = forms.CharField(label="Titulo:", max_length=200)
     descripcion = forms.CharField(label = "Descripcion.:", widget=forms.Textarea)
+
+class login(forms.Form):
+    usuario = forms.CharField(label="Usuario:", max_length=50)
+    contrasena = forms.CharField(label = "Contraseña:", max_length=50)
